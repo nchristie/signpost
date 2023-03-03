39c537908e41d636597c6e9f07cfbb2c5347484d
Naomi Christie <naomi.christie@gmail.com>
2023-03-03 03:59:53 +0000

    Add validation to vote table
    Prevents votes being created when the url differs between category and
    term.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,14 @@
 class Vote < ApplicationRecord
   belongs_to :url_category
   belongs_to :url_term
+
+  validate :video_url_match
+
+  private
+
+  def video_url_match
+    return if url_term.video_url == url_category.video_url
+
+    errors.add(:base, 'UrlTerm and UrlCategory must have the same VideoUrl')
+  end
 end
