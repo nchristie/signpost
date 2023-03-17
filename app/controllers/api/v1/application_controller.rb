738ddc17b8a5eafa86f4ae91b4a414e4231c912a
Naomi Christie <naomi.christie@gmail.com>
2023-03-17 13:21:53 +0000

    Allow cors

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,8 +1,14 @@
-# frozen_string_literal: true
-
 module Api
   module V1
     class ApplicationController < ActionController::API
+      before_action :allow_cors
+
+      def allow_cors
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+        response.headers['Access-Control-Max-Age'] = '1728000'
+      end
     end
   end
 end
